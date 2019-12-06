ea10024701e4eda48877147f5b75cdd22602ace1
heihei <2280293428@qq.com>
2019-12-06 20:51:14 +0800

    初始化项目

---
+++ maven_day02_demo/src/main/java/com/tz/demo/Test.java
@@ -0,0 +1,10 @@
+package com.tz.demo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+        System.out.println("222222222222");
+        System.out.println("333333333333333");
+    }
+}
---
+++ maven_day02_demo/src/main/java/com/tz/demo/Test1.java
@@ -0,0 +1,10 @@
+package com.tz.demo;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("111111111111111");
+
+    }
+}
